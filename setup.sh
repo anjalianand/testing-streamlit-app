a703202a6fb4859a4d957008ae2857a922dcf9c2
anjalianand <68214511+anjalianand@users.noreply.github.com>
2020-07-14 20:21:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"anjalianand0921@gmail.com\"\n\
+email = \"i.anjalianand@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
